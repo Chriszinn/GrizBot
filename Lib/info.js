4c9003e25cd06d79b4783de1a0f23eb57ddda834
Chriszin <76415134+Chriszinn@users.noreply.github.com>
2020-12-22 20:38:54 -0300

    Add files via upload

---
+++ Lib/info.js
@@ -0,0 +1,38 @@
+exports.info = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*${A187}*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT ATIVO: *${aktif}*
+
+         
+╔════════════════════
+║ *Feito por: $Chriszin*
+╠════════════════════
+║   *⚠️ATENÇÃO⚠️*
+╠════════════════════
+║*OS CRÉDITOS NAO SÃO MEUS*
+║*APENAS FIZ UMA VERSÃO INSTÁVEL*
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│➸ *Faça login aqui😙*
+║│ _${whatsapp}_
+║│➸ *YouTube*
+║│ _${nomer}_
+║│➸ *Instagram <Segue>*
+║│ _${instagram}_
+║│➸ *Criador*
+║│    Chriszin ツ
+║╰───────────
+║         ${A187}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${A187}
+╠════════════════════
+║ _*Feito por Chriszin ツ*_
+╚════════════════════`
+}
